974f13c487194182bec5f028e31ed8d3f83efc72
BigBoiChickenNugget <talha05ahmad@gmail.com>
2024-10-02 15:55:42 -0400

    script to update my dots plus some random stuff

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# Update the dots
+cp -r ~/.config/alacritty .
+cp -r ~/.config/fish .
+cp -r ~/.config/hypr .
+cp -r ~/.config/nvim .
+cp -r ~/.config/polybar .
+cp -r ~/.config/rofi .
+cp -r ~/.config/sway .
+cp -r ~/.config/waybar .
+cp -r ~/.config/zathura .
+cp -r ~/.config/tmux .
+cp -r ~/.config/wofi .
+cp -r ~/.config/i3 .
